f07660bc7c5c8908d349b10df45ea27d37a7b6e0
alexsabdev <alexsabdev@gmail.com>
2016-06-26 19:09:19 +0300

    added popup folder

---
+++ popup/popup.js
@@ -0,0 +1,10 @@
+(function() {
+	'use strict';
+
+	var backgroundpage= chrome.extension.getBackgroundPage();
+	document.querySelector('button').addEventListener('click', function() {
+		var start = document.querySelector('#start').value;
+		var end = document.querySelector('#end').value;
+		backgroundpage.handleButtonClick(start, end);
+	});
+})();
